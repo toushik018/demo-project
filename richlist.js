b50d9d220ecb5b6eae98ee830fb4eee10b411572
Toushik <tayebhossain018@gmail.com>
2023-08-20 15:55:10 +0600

    performing lood on the apie

--- richlist.js
+++ richlist.js
@@ -1,55 +1,66 @@
 document.addEventListener('DOMContentLoaded', async function () {
-    const response = await fetch('https://api.hive-engine.com/rpc/contracts', {
-        method: 'POST',
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0',
-            'Accept': 'application/json, text/plain, */*',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Origin': 'https://he.dtools.dev',
-            'Connection': 'keep-alive',
-            'Referer': 'https://he.dtools.dev/',
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'cross-site'
-        },
-        body: JSON.stringify({
-            'jsonrpc': '2.0',
-            'id': 1692187530638,
-            'method': 'find',
-            'params': {
-                'contract': 'tokens',
-                'table': 'balances',
-                'query': {
-                    'symbol': 'BEE'
-                },
-                'offset': 0,
-                'limit': 1000
-            }
-        })
-    });
-
-    const data = await response.json();
     const tableBody = document.getElementById('tableBody');
+    
+    async function fetchData(offset) {
+        const response = await fetch('https://api.hive-engine.com/rpc/contracts', {
+            method: 'POST',
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0',
+                'Accept': 'application/json, text/plain, */*',
+                'Accept-Language': 'en-US,en;q=0.5',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Origin': 'https://he.dtools.dev',
+                'Connection': 'keep-alive',
+                'Referer': 'https://he.dtools.dev/',
+                'Sec-Fetch-Dest': 'empty',
+                'Sec-Fetch-Mode': 'cors',
+                'Sec-Fetch-Site': 'cross-site'
+            },
+            body: JSON.stringify({
+                'jsonrpc': '2.0',
+                'id': 1692187530638,
+                'method': 'find',
+                'params': {
+                    'contract': 'tokens',
+                    'table': 'balances',
+                    'query': {
+                        'symbol': 'SPS'
+                    },
+                    'offset': offset,
+                    'limit': 1000
+                }
+            })
+        });
+        
+        const data = await response.json();
+        return data.result;
+    }
+    
+    let offset = 0;
+    let newData;
+    
+    do {
+        newData = await fetchData(offset);
 
-    console.log(data);
-    data.result.forEach((balance, index) => {
-        const Balance = parseFloat(balance.balance).toFixed(3);
-        const totalBalance = parseFloat(balance.balance + balance.stake).toFixed(3);
-    
-        const newRow = document.createElement('tr');
-        newRow.innerHTML = `
-            <td class="py-2 px-4 ${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}">${index + 1}</td>
-            <td class="py-2 px-4 user-name-cell ${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}">${balance.account}</td>
-            <td class="py-2 px-4 ${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}">${Balance}</td>
-            <td class="py-2 px-4 ${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}">${balance.stake}</td>
-            <td class="py-2 px-4 ${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}">${totalBalance}</td>
-        `;
-        tableBody.appendChild(newRow);
-    });
-    
+        console.log(newData);
+        
+        newData.forEach((balance, index) => {
+            const newRow = document.createElement('tr');
+            newRow.innerHTML = `
+                <td class="py-2 px-4">${index + 1 + offset}</td>
+                <td class="py-2 px-4 user-name-cell">${balance.account}</td>
+                <td class="py-2 px-4">${balance.balance}</td>
+                <td class="py-2 px-4">${balance.stake}</td>
+                <td class="py-2 px-4">${balance.balance + balance.stake}</td>
+            `;
+            tableBody.appendChild(newRow);
+        });
+        
+        offset += 1000;
+    } while (newData.length > 0);
+
 
 
 
@@ -142,10 +153,4 @@ function searchUser() {
     }
 }
 
-
 });
-
-
-
-
-
