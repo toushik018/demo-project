01c056fb505b427144818790fe6fb52ccfa7ae33
toushik018 <tayebhossain018@gmail.com>
2023-09-18 10:04:52 +0600

    fix responsive issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,14 @@ module.exports = {
     },
     container: {
       center: true,
-    }
+    },
+    screens: {
+      'sm': '640px',      // Small screens like smartphones
+      'md': '768px',      // Medium screens like tablets
+      'lg': '1024px',     // Large screens like laptops
+      'xl': '1280px',     // Extra-large screens like desktops
+      '2xl': '1536px',    // Larger desktop screens
+    },
   },
   plugins: [],
 }
